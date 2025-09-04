26cc570dea7aa9cd80c718bf2b4b4de60b1b7e7e
MaryomaElsayed <maryamelsayd.1@gmail.com>
2025-09-04 13:09:13 +0300

    Fixing Errors

--- 01_Weather-App/script.js
+++ 01_Weather-App/script.js
@@ -1,24 +1,32 @@
 const apiKey = "ef62cc6b5e9be8b6786a7dd87978dca2";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Alexandria";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather(){
-	const response = await fetch(apiUrl + `&appid=${apiKey}`);
-	var data = await response.json();
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+	const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+	const data = await response.json();
 
 	console.log(data);
 
-	// update the dom let's goooooooooooooo !
-	// يالى ابتديت الحب معااااك عمرى ابتدى و انا ويااااااااااك
-	document.querySelector(".city").innerHTML = data.name;
+	let cityName = data.name;
+	if (cityName.toLowerCase() === "cairo") {
+		cityName = "Cairo ❤️";
+		document.body.style.backgroundImage = "url('images/cairo.jpg')";
+    	document.body.style.backgroundSize = "cover";
+    	document.body.style.backgroundPosition = "center"; 
+	}
+
+	document.querySelector(".city").innerHTML = cityName;
 	document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-	// قال ايييييييييه بيسالونى 😏
-	// عنك يا نور عيونى 🌚
-	// معقول اكون بحبك (الصراحه اه)
 	document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 	document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
-
-
 }
 
+searchBtn.addEventListener("click", ()=>{
+	checkWeather(searchBox.value);
+});
 
-checkWeather();
+// Default city
+checkWeather("Alexandria");
