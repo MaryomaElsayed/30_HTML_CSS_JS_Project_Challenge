815df78cf3897dcd66e4db69ed9f08df62f58d30
MaryomaElsayed <maryamelsayd.1@gmail.com>
2025-09-07 07:08:47 +0300

    Advanced Project

--- 04_Notes/script.js
+++ 04_Notes/script.js
@@ -2,14 +2,35 @@
  const createBtn = document.querySelector(".btn");
  let notes = document.querySelectorAll(".input-box");
 
+ const noNotesMsg = document.getElementById("no-notes");
+
+
 
 createBtn.addEventListener("click", ()=>{
+
+	// remove "No Notes yet" if it exists
+    if (noNotesMsg) {
+        noNotesMsg.remove();
+    }
+
+
 	let inputBox = document.createElement("p");
 	let img = document.createElement("img");
 	inputBox.className = "input-box";
 	inputBox.setAttribute("contenteditable" , "true");
 	img.src = "images/trash.png";
 	notesContainer.appendChild(inputBox).appendChild(img);
+
+	 // delete functionality
+    img.addEventListener("click", () => {
+        inputBox.remove();
+
+        // if no notes left, show "No Notes yet" again
+        if (notesContainer.querySelectorAll(".input-box").length === 0) {
+            notesContainer.innerHTML = `<p id="no-notes">No Notes yet</p>`;
+        }
+    });
+    
 })
 
 notesContainer.addEventListener("click" , function(m){
@@ -17,3 +38,19 @@ notesContainer.addEventListener("click" , function(m){
 		m.target.parentElement.remove();
 	}
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
